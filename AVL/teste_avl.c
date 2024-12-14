34874277c0d1696c3dfbc7182595b41749cdd316
JorgRibeiro <yorkis.gamer32@gmail.com>
2024-12-13 23:59:39 -0300

    Comentando o Codigo

--- AVL/teste_avl.c
+++ AVL/teste_avl.c
@@ -11,21 +11,24 @@ int main(int argc, char * argv[]) {
         int valor;
         scanf("%d", &opcao);
         switch(opcao) {
-            //inserir
+            //Inserir
             case 1:
                 scanf("%d", &valor);
                 arvore = avl_inserir(arvore, valor);
                 break;
             case 2:
+            //Pre-order
                 avl_preorder(arvore); 
                 printf("\n");
                 break;
            case 3:
+           //Remover
                 scanf("%d", &valor);
                 arvore = avl_remover(arvore, valor);
                 printf("\n");
                 break;
             case 99:
+            //Sair
                 exit(0);
         }    
     }
--- BST/teste_bst.c
+++ BST/teste_bst.c
@@ -3,46 +3,51 @@
 #include "bst.h"
 
 int main(int argc, char * argv[]) {
-    //Declarar uma variável do tipo árvore
-    Raiz arvore;
-    //inicializar como uma árvore vazia
-    arvore = NULL;
+    //Declarar uma variável do tipo árvore e inicializar como uma árvore vazia
+    Raiz arvore = NULL;
     
     while(1) {
         int opcao;
         int valor;
         scanf("%d", &opcao);
         switch(opcao) {
-            //inserir
+            //Inserir
             case 1:
                 scanf("%d", &valor);
                 arvore = bst_inserir(arvore, valor);
                 break;
             case 2:
+            //Pre-order
                 bst_preorder(arvore); 
                 printf("\n");
                 break;
            case 3:
+           //In-order
                 bst_inorder(arvore); 
                 printf("\n");
                 break;
             case 4:
+            //Pos-order
                 bst_posorder(arvore); 
                 printf("\n");
                 break;
             case 5:
+            //Reverso
                 bst_reverso(arvore); 
                 printf("\n");
                 break;
             case 6:
+            //Altura
                 printf("%d\n",bst_altura(arvore));
                 printf("\n");
                 break;
             case 7:
+            //Quantidade de folhas de uma arvore
                 printf("%d\n",bst_quant_folhas(arvore));
                 printf("\n");
                 break;
             case 8:
+            //Caminho até uma raiz
                 scanf("%d", &valor);
                 if(bst_valida_caminho(arvore,valor) == 1){
                     bst_caminho(arvore, valor);    
@@ -50,11 +55,13 @@ int main(int argc, char * argv[]) {
                 printf("\n");
                 break;
             case 9:
+            //Remover
                 scanf("%d", &valor);
                 arvore = bst_remover(arvore, valor);
                 printf("\n");
                 break;
             case 99:
+            //sair
                 exit(0);
         }    
     }
