4b09a53128727be8340734e6cefb39bef3d32884
JorgRibeiro <yorkis.gamer32@gmail.com>
2024-12-20 15:33:47 -0300

    add quantidade de nos pretos

--- TREE/RED-BLACK/rbt.c
+++ TREE/RED-BLACK/rbt.c
@@ -4,51 +4,71 @@
 
 Raiz rbt_inserir(Raiz raiz, int novo_valor)
 {
-    if (raiz == NULL) {                                      // Se a arvore for vazia, Inicializa a raiz
+    if (raiz == NULL)
+    { // Se a arvore for vazia, Inicializa a raiz
         Raiz novo = (Raiz)malloc(sizeof(no));
         novo->chave = novo_valor;
-        novo->cor = 'b';                                    // Define a cor da raiz como PRETA
+        novo->cor = 'b'; // Define a cor da raiz como PRETA
         novo->raiz_esq = NULL;
         novo->raiz_dir = NULL;
         novo->raiz_pai = NULL;
         return novo;
     }
-    else if (novo_valor > raiz->chave)                      // Se o novo_valor for maior que a chave 
+    else if (novo_valor > raiz->chave) // Se o novo_valor for maior que a chave
     {
-        if (raiz->raiz_dir == NULL)                         // Se não existir um DIREITO nesse NO 
+        if (raiz->raiz_dir == NULL) // Se não existir um DIREITO nesse NO
         {
             Raiz novo = (Raiz)malloc(sizeof(no));
             raiz->raiz_dir = novo;
             novo->chave = novo_valor;
-            novo->cor = 'r';                                 // Define a cor do no como Vermelha
+            novo->cor = 'r'; // Define a cor do no como Vermelha
             novo->raiz_esq = NULL;
             novo->raiz_dir = NULL;
             novo->raiz_pai = raiz;
         }
-        else                                               // Se existir um node DIREITO nesse NO     
+        else // Se existir um node DIREITO nesse NO
         {
-            raiz->raiz_dir = rbt_inserir(raiz->raiz_dir, novo_valor);        // Ele vai continuar a busca, ate um no vazio
+            raiz->raiz_dir = rbt_inserir(raiz->raiz_dir, novo_valor); // Ele vai continuar a busca, ate um no vazio
         }
     }
-    else if ((novo_valor < raiz->chave))                     // Se o novo_valor for menor que a chave 
+    else if ((novo_valor < raiz->chave)) // Se o novo_valor for menor que a chave
     {
-        if (raiz->raiz_esq == NULL)                          // Se não existir um ESQUERDO nesse NO 
+        if (raiz->raiz_esq == NULL) // Se não existir um ESQUERDO nesse NO
         {
             Raiz novo = (Raiz)malloc(sizeof(no));
             raiz->raiz_esq = novo;
             novo->chave = novo_valor;
-            novo->cor = 'r';                                 // Define a cor do no como Vermelha
+            novo->cor = 'r'; // Define a cor do no como Vermelha
             novo->raiz_esq = NULL;
             novo->raiz_dir = NULL;
             novo->raiz_pai = raiz;
         }
         else
         {
-            raiz->raiz_esq = rbt_inserir(raiz->raiz_esq, novo_valor);         // Ele vai continuar a busca, ate um no vazio       
+            raiz->raiz_esq = rbt_inserir(raiz->raiz_esq, novo_valor); // Ele vai continuar a busca, ate um no vazio
         }
     }
 
     return raiz;
+}
+
+Raiz rbt_ajuste(Raiz raiz)
+{
+    return raiz;
+}
+
+int rbt_quant_preto(Raiz raiz)
+{
+    if (raiz != NULL)
+    {
+        return rbt_quant_preto(raiz->raiz_dir) - rbt_quant_preto(raiz->raiz_esq);
+        if(raiz->cor == 'b'){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    return 0;
 }
 
 void rbt_preorder(Raiz raiz)
@@ -122,14 +142,18 @@ void rbt_info(Raiz raiz, int valor)
                 printf("Pai: NULL\n");
             else
                 printf("Pai: %d\n", raiz->raiz_pai->chave);
+            printf("Quantidade de nos pretos: %d\n",rbt_quant_preto(raiz));
         }
     }
 }
 
-Raiz rbt_maior_valor(Raiz raiz){
-    if(raiz!= NULL){
+Raiz rbt_maior_valor(Raiz raiz)
+{
+    if (raiz != NULL)
+    {
         Raiz procura = raiz;
-        while(procura->raiz_dir != NULL){
+        while (procura->raiz_dir != NULL)
+        {
             procura = procura->raiz_dir;
         }
         return procura;
@@ -139,34 +163,44 @@ Raiz rbt_maior_valor(Raiz raiz){
 
 Raiz rbt_remover(Raiz raiz, int valor_removido)
 {
-    if(raiz != NULL){
-        if(valor_removido > raiz->chave){
+    if (raiz != NULL)
+    {
+        if (valor_removido > raiz->chave)
+        {
             raiz->raiz_dir = rbt_remover(raiz->raiz_dir, valor_removido);
-        } else if(valor_removido < raiz->chave){
+        }
+        else if (valor_removido < raiz->chave)
+        {
             raiz->raiz_esq = rbt_remover(raiz->raiz_esq, valor_removido);
-        } else {
-            if(raiz->raiz_dir == NULL && raiz->raiz_esq == NULL){
+        }
+        else
+        {
+            if (raiz->raiz_dir == NULL && raiz->raiz_esq == NULL)
+            {
                 free(raiz);
                 return NULL;
-            } else if(raiz->raiz_dir != NULL && raiz->raiz_esq == NULL){
+            }
+            else if (raiz->raiz_dir != NULL && raiz->raiz_esq == NULL)
+            {
                 raiz->raiz_dir->raiz_pai = raiz->raiz_pai;
                 Raiz temp = raiz->raiz_dir;
                 free(raiz);
                 return temp;
-            } else if(raiz->raiz_dir == NULL && raiz->raiz_esq != NULL){
+            }
+            else if (raiz->raiz_dir == NULL && raiz->raiz_esq != NULL)
+            {
                 raiz->raiz_esq->raiz_pai = raiz->raiz_pai;
                 Raiz temp = raiz->raiz_esq;
                 free(raiz);
                 return temp;
-            } else{
+            }
+            else
+            {
                 raiz->chave = rbt_maior_valor(raiz->raiz_esq)->chave;
                 raiz->raiz_esq = rbt_remover(raiz->raiz_esq, raiz->chave);
                 return raiz;
             }
-
-
         }
-
     }
     return raiz;
 }
