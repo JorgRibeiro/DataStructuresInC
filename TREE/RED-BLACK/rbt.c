18c6fd80d5d06f2faa58d2c24c2003408c5a8826
JorgRibeiro <yorkis.gamer32@gmail.com>
2024-12-23 20:17:56 -0300

    Ajuste - não acabado

--- TREE/RED-BLACK/rbt.c
+++ TREE/RED-BLACK/rbt.c
@@ -68,9 +68,10 @@ Raiz rbt_ajuste(Raiz raiz, Raiz novo)
         if(posiciona->cor == 'r' && pai->cor == 'r' ){          // quebra da regra, Pai e filho vermelhos
             if(posiciona == pai->raiz_dir){                     // posiciona é igual ao filho direito de pai
                 Raiz Tio;                                       // Tio é criado
+                Raiz irmao = pai->raiz_esq;
                 if(pai == vo->raiz_dir){                        // Pai é igual ao filho direito de vo
                     Tio = vo->raiz_esq;                         // Tio é esquerdo de vo
-                    if(Tio == NULL){                            // rotação simples esquerda
+                    if(Tio == NULL || Tio->cor == 'b'){                            // rotação simples esquerda
                         rbt_rotacao_simples_esq(vo);
                         if(vo == raiz){
                             return pai;
@@ -84,7 +85,7 @@ Raiz rbt_ajuste(Raiz raiz, Raiz novo)
                 } else {                                        // Pai é igual ao filho esquerdo de vo
                     Tio = vo->raiz_dir;  
 
-                     if(Tio == NULL){                            // rotação simples esquerda
+                     if(Tio == NULL || Tio->cor == 'b'){                            // rotação simples esquerda
                         rbt_rotacao_simples_esq(pai);
                         rbt_rotacao_simples_dir(vo);
                         if(vo == raiz){
@@ -98,23 +99,41 @@ Raiz rbt_ajuste(Raiz raiz, Raiz novo)
                 }
             } else {                                            // posiciona é igual ao filho esquerdo de pai
                 Raiz Tio;
-                if(pai == vo->raiz_dir){
+                Raiz irmao = pai->raiz_dir;
+                if(pai == vo->raiz_esq){
+                    Tio = vo->raiz_dir;                         // Tio é esquerdo de vo
+                    if(Tio == NULL || Tio->cor == 'b'){                            // rotação simples esquerda
+                        rbt_rotacao_simples_dir(vo);
+                        if(vo == raiz){
+                            return pai;
+                        }
+                    } else {
+                        Tio->cor = 'b';
+                        pai->cor = 'b';
+                        if(vo->raiz_pai != NULL){ vo->cor = 'r'; }  // se Vo não for a raiz
+                    }
+     
+                } else {
+                     if(Tio == NULL || Tio->cor == 'b'){
                         rbt_rotacao_simples_dir(pai);            // rotação dupla esquerda
                         rbt_rotacao_simples_esq(vo);   
                         if(vo == raiz){
                             return posiciona;
-                        }    
-                }
+                        }       
+                     } else {
+                        Tio->cor = 'b';
+                        pai->cor = 'b';
+                        if(vo->raiz_pai != NULL){ vo->cor = 'r'; }  // se Vo não for a raiz
+                     }     
             }
 
 
         }
 
-        posiciona = posiciona->raiz_pai;
     }
     
-    
-
+        posiciona = posiciona->raiz_pai;
+    }
     return raiz;
 }
 
