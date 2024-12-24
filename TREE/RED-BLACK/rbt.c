44d9c913273e9f894c44d7834f7ee30ab54319cc
JorgRibeiro <yorkis.gamer32@gmail.com>
2024-12-24 04:26:30 -0300

    testes

--- TREE/RED-BLACK/rbt.c
+++ TREE/RED-BLACK/rbt.c
@@ -68,7 +68,6 @@ Raiz rbt_ajuste(Raiz raiz, Raiz novo)
         if(posiciona->cor == 'r' && pai->cor == 'r' ){          // quebra da regra, Pai e filho vermelhos
             if(posiciona == pai->raiz_dir){                     // posiciona é igual ao filho direito de pai
                 Raiz Tio;                                       // Tio é criado
-                Raiz irmao = pai->raiz_esq;
                 if(pai == vo->raiz_dir){                        // Pai é igual ao filho direito de vo
                     Tio = vo->raiz_esq;                         // Tio é esquerdo de vo
                     if(Tio == NULL || Tio->cor == 'b'){                            // rotação simples esquerda
@@ -76,12 +75,11 @@ Raiz rbt_ajuste(Raiz raiz, Raiz novo)
                     } else {
                         Tio->cor = 'b';
                         pai->cor = 'b';
-                        if(vo->raiz_pai != NULL){ vo->cor = 'r'; }  // se Vo não for a raiz
-                       
+                        if(vo->raiz_pai != NULL){ vo->cor = 'r'; }  // se Vo não for a raiz                 
                     }
-                } else {                                        // Pai é igual ao filho esquerdo de vo
+                } else if(pai == vo->raiz_esq) {                                        // Pai é igual ao filho esquerdo de vo
                     Tio = vo->raiz_dir;  
-                     if(Tio == NULL || Tio->cor == 'b'){                            // rotação simples esquerda
+                     if(Tio == NULL || Tio->cor == 'b'){                // rotação dupla direita                    
                         rbt_rotacao_simples_esq(pai);
                         rbt_rotacao_simples_dir(vo);
                         if(posiciona->raiz_pai == NULL){return posiciona;}
@@ -93,9 +91,9 @@ Raiz rbt_ajuste(Raiz raiz, Raiz novo)
                 }
             } else {                                            // posiciona é igual ao filho esquerdo de pai
                 Raiz Tio;
-                Raiz irmao = pai->raiz_dir;
                 if(pai == vo->raiz_esq){
                     Tio = vo->raiz_dir;                         // Tio é esquerdo de vo
+
                     if(Tio == NULL || Tio->cor == 'b'){                            // rotação simples esquerda
                         rbt_rotacao_simples_dir(vo);
                     } else {
@@ -105,9 +103,9 @@ Raiz rbt_ajuste(Raiz raiz, Raiz novo)
                     }
      
                 } else {
-                     if(Tio == NULL || Tio->cor == 'b'){
-                        rbt_rotacao_simples_dir(pai);            // rotação dupla esquerda
-                        rbt_rotacao_simples_esq(vo);      
+                     if(Tio == NULL || Tio->cor == 'b'){        // rotação dupla esquerda
+                        rbt_rotacao_simples_dir(pai);        
+                        rbt_rotacao_simples_esq(vo);   
                          if(posiciona->raiz_pai == NULL){return posiciona;}
                      } else {
                         Tio->cor = 'b';
@@ -115,7 +113,6 @@ Raiz rbt_ajuste(Raiz raiz, Raiz novo)
                         if(vo->raiz_pai != NULL){ vo->cor = 'r'; }  // se Vo não for a raiz
                      }     
             }
-
 
         }
 
