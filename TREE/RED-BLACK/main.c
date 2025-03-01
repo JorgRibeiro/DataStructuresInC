8872351d4b22819855959b9830144e1bf97dc161
Jorge Ribeiro <102694110+JorgRibeiro@users.noreply.github.com>
2025-03-01 00:56:56 -0300

    Update main.c

--- TREE/RED-BLACK/main.c
+++ TREE/RED-BLACK/main.c
@@ -23,31 +23,9 @@ int main(int argc, char * argv[]) {
 						break;
 						
 				case 3:
-						printf("%d\n", maior_elemento(a));
-						break;
-				case 4:
-						printf("%d\n", menor_elemento(a));
-						break;
-				case 5:
-				                printf("%d\n", altura(a));
-						break;
-				case 6:
-						in_order(a);
-						printf("\n");
-						break;
-				case 7:
-						pos_order(a);
-						printf("\n");
-						break;
-				case 8:
 						scanf("%d", &valor);
 						remover(valor, &a);
 						break;
-				case 9:
-						imprimir(a);
-						printf("\n");
-						break;
-
 				case 99:
 						exit(0);
 		}
